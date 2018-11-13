376191d77aa2cf0312bbea8fd0dc9090e2631732
Omkar Deshpande <omkar.deshpande1@vodafone.com>
2018-11-13 17:00:13 +0530

    adding new code

--- src/test/java/Test/Test/AppTestTest.java
+++ src/test/java/Test/Test/AppTestTest.java
@@ -1,18 +1,10 @@
 package Test.Test;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class AppTestTest {
 
 	public void hi(){
 		System.out.println("This is for CICD");
 	}
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-		
-	}
+	
 
 }
