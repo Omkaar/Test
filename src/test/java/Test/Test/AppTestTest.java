95e3ea048da5f741887a600837f2972f1699e05b
Omkar Deshpande <omkar.deshpande1@vodafone.com>
2018-11-13 16:53:22 +0530

    adding new changes to hi for testing cicd

---
+++ src/test/java/Test/Test/AppTestTest.java
@@ -0,0 +1,18 @@
+package Test.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTestTest {
+
+	public void hi(){
+		System.out.println("This is for CICD");
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		
+	}
+
+}
